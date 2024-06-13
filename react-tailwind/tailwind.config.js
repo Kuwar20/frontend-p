2c891aa885ba91ed487118d1a4cdcf54677201e1
Kuwar20 <kuwarx1@gmail.com>
2024-06-13 19:15:16 +0530

    style: Update font and color styles
    - Update font styles by importing the 'Jost' font from Google Fonts
    - Set the font family for the entire body to 'Jost', sans-serif
    - Update the color of the 'Hello World' heading to use the 'primary' color defined in the Tailwind CSS theme

--- react-tailwind/tailwind.config.js
+++ react-tailwind/tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'primary': '#010851',
+        'secondary': '#9A7AF1',
+        'tertiary': '#707070',
+        'pink': '#EE9AE5',
+      }
+    },
   },
   plugins: [],
 }
