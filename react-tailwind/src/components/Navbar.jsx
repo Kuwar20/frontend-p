874b19bdcf94c115732fe306cfebf9483fa732ca
Kuwar20 <kuwarx1@gmail.com>
2024-06-14 10:05:04 +0530

    chore: Add mobile menu functionality to Navbar component

--- react-tailwind/src/components/Navbar.jsx
+++ react-tailwind/src/components/Navbar.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from '../assets/logo.png'
 import { MdLanguage } from "react-icons/md";
+import { FaXmark } from "react-icons/fa6";
+import { FaBars } from "react-icons/fa";
+
 
 const Navbar = () => {
+
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = ()=>{
+    setMenuOpen(!isMenuOpen);
+  }
 
   const navItems = [
     { link: "Overview", path: "home" },
@@ -19,19 +28,26 @@ const Navbar = () => {
           <img className='w-10 inline-block items-center' src={logo} alt="logo" /><span>XYZ</span>
         </a>
 
-        {/* Nav Items */}
-        <ul className='md:flex space-x-12'>
+        {/* Nav Items */} {/* hide the navbar content using hidden */}
+        <ul className='hidden md:flex space-x-12'>
           {
             navItems.map(({link, path}) => <a key={link} href={path} className='block hover:text-gray-300'>{link}</a>)
           }
         </ul>
         </div>
-        <div className='space-x-12 flex items-center'>
+        <div className='space-x-12 hidden md:flex items-center'>
           <a href="/" className='hidden lg:flex items-center hover:text-secondary'><MdLanguage className='mr-2'/>Languages</a>
           <button className='bg-secondary px-2 py-2 transition-all duration-300 rounded hover:text-white hover:bg-indigo-600'>Signout</button>
         </div>
 
         {/* Menu btn, only display on Mobile */}
+        <div className="md:hidden">
+          <button onClick={toggleMenu} className='text-primary focus:outline-none focus:text-gray-300'>
+            {
+                isMenuOpen ?  (<FaXmark className='w-5 h-6 text-primary'/>): (<FaBars/>)
+            }
+          </button>
+        </div>
       </div>
     </nav>
   )
