289258c99777af9c7b2d1711f61b78f12b45d0ba
Kuwar20 <kuwarx1@gmail.com>
2024-06-17 13:30:47 +0530

    feat: Add form submission functionality to Form component
    This commit adds form submission functionality to the Form component. It includes the necessary code to handle form submission, including sending the user's email, name, age, and gender to the API using the createUser action from the userDetailSlice. The handleSubmit function is triggered when the form is submitted, preventing the default form submission behavior and dispatching the createUser action with the user data. This allows the application to create a new user and update the userDetail state with the new user data.

--- react-redux/src/components/Form.jsx
+++ react-redux/src/components/Form.jsx
@@ -1,30 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux';
+import { createUser } from '../features/userDetailSlice';
 
 const Form = () => {
+
+    const [users, setUsers] = useState({});
+    const dispatch = useDispatch();
+
+    const getUserData = (e) => {
+        setUsers({ ...users, [e.target.name]: e.target.value })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(users);
+        dispatch(createUser(users));
+    };
+
     return (
         <div>
-            <form className='w-50 mx-auto my-5'>
-                <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Email</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                </div>
-                <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Name</label>
-                    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                </div>
-                <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Age</label>
-                    <input type="number" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+            <form className='w-50 mx-auto my-5' onSubmit={handleSubmit}>
+                <div className="mb-3">
+                    <label for="exampleInputEmail1" className="form-label">Email</label>
+                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+                        name="email"
+                        onChange={getUserData}
+                    />
                 </div>
                 <div className="mb-3">
-                    <input type="radio" name="gender" className="form-check-input" id="male" />
+                    <label for="exampleInputEmail1" className="form-label">Name</label>
+                    <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+                        name='name'
+                        onChange={getUserData}
+                    />
+                </div>
+                <div className="mb-3">
+                    <label for="exampleInputEmail1" className="form-label">Age</label>
+                    <input type="number" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+                        name='age'
+                        onChange={getUserData}
+                    />
+                </div>
+                <div className="mb-3">
+                    <input type="radio" name="gender" className="form-check-input" id="male"
+                        value="Male"
+                        onChange={getUserData}
+                    />
                     <label className="form-check-label" htmlFor="male">Male</label>
                 </div>
                 <div className="mb-3">
-                    <input type="radio" name="gender" className="form-check-input" id="female" />
+                    <input type="radio" name="gender" className="form-check-input" id="female"
+                        value='Female'
+                        onChange={getUserData}
+                    />
                     <label className="form-check-label" htmlFor="female">Female</label>
                 </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <button type="submit" className="btn btn-primary">Submit</button>
             </form></div>
     )
 }
--- react-redux/src/features/userDetailSlice.js
+++ react-redux/src/features/userDetailSlice.js
@@ -1,11 +1,44 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+// create action
+export const createUser = createAsyncThunk("createUser", async (data, { rejectWithValue }) => {
+    const response = await fetch("https://666fc0fe0900b5f872481dcc.mockapi.io/crud", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+    });
+
+    try {
+        const result = await response.json();
+        return result;
+    } catch (error) {
+        return rejectWithValue(error.response);
+    }
+});
 
 export const userDetail = createSlice({
     name: "userDetail",
     initialState: {
-        user: [],
+        users: [],
         loading: false,
         error: null,
+    },
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(createUser.pending, (state) => {
+                state.loading = true;
+            })
+            .addCase(createUser.fulfilled, (state, action) => {
+                state.loading = false;
+                state.users.push(action.payload);
+            })
+            .addCase(createUser.rejected, (state, action) => {
+                state.loading = false;
+                state.error = action.payload;
+            });
     },
 });
 
