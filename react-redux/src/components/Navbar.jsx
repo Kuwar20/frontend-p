d440938a1e7fdc2fb98310a789d20dbd6c147700
Kuwar20 <kuwarx1@gmail.com>
2024-06-17 10:44:46 +0530

    feat: Update landing page banner color and add lazy loading for images

---
+++ react-redux/src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+function Navbar() {
+    return (
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+                    <a class="navbar-brand" href="#">Redux Brand</a>
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Create Post</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">All Post</a>
+                        </li>
+                    </ul>
+                    <form class="d-flex" role="search">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                        <button class="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
