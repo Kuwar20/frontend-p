9e1d509f05ed2525e576016973b6ac9dbac703fd
Kuwar20 <kuwarx1@gmail.com>
2024-06-22 21:54:05 +0530

    feat: Add Login and Signup pages
    This commit adds the Login and Signup pages to the application. The Login page is implemented in the "Login.jsx" file, and the Signup page is implemented in the "Signup.jsx" file. These pages provide the necessary UI and functionality for users to log in and sign up. This addition enhances the overall user experience by allowing users to access and interact with these important features.

--- react-redux-tailwind-sinlog/src/App.jsx
+++ react-redux-tailwind-sinlog/src/App.jsx
@@ -2,6 +2,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Navbar from "./components/Navbar"
 import Notfound from "./pages/Notfound"
 import Home from "./pages/Home"
+import Login from "./pages/Login"
+import Signup from "./pages/Signup"
+
 function App() {
 
   return (
@@ -10,6 +13,8 @@ function App() {
       <Navbar />  
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
           <Route path="*" element={<Notfound/>} />
         </Routes>
       </BrowserRouter>
