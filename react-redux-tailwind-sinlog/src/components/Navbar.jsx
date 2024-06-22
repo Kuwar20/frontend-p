e87e39a40a1d6d6a207ab6cebcada3b4a310097a
Kuwar20 <kuwarx1@gmail.com>
2024-06-22 21:49:56 +0530

    feat: Add react-icons package and implement icons in Navbar component
    This commit adds the "react-icons" package as a dependency and imports the necessary icons in the Navbar component. The FaBars and FaTimes icons from the react-icons library are used to represent the open and close icons for the mobile navigation menu. This addition enhances the visual appeal and functionality of the Navbar component.

--- react-redux-tailwind-sinlog/src/components/Navbar.jsx
+++ react-redux-tailwind-sinlog/src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa';
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -6,8 +7,8 @@ const Navbar = () => {
   return (
     <nav className='bg-slate-400 p-4 text-xl font-semibold'>
       <div className='flex items-center justify-between'>
-        <div className='text-2xl'>
-          <a href="/" className='hover:text-slate-600'>Brand Name</a>
+        <div>
+          <a className='hover:text-slate-600'>Brand Name</a>
         </div>
         <div className='hidden md:flex space-x-5 ml-14'>
           <a href="/" className='hover:text-slate-600'>Home</a>
@@ -19,9 +20,7 @@ const Navbar = () => {
         </div>
         <div className='md:hidden'>
           <button onClick={() => setIsOpen(!isOpen)} className='focus:outline-none'>
-            <svg className='w-6 h-6' fill='none' stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
-              <path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d={isOpen ? 'M6 18L18 6M6 6l12 12' : 'M4 6h16M4 12h16m-7 6h7'}></path>
-            </svg>
+            {isOpen ? <FaTimes className='w-6 h-6' /> : <FaBars className='w-6 h-6' />}
           </button>
         </div>
       </div>
@@ -30,7 +29,7 @@ const Navbar = () => {
           <a href="/" className='block py-2 hover:text-slate-600'>Home</a>
           <a href="/login" className='block py-2 hover:text-slate-600'>Login</a>
           <a href="/signup" className='block py-2 hover:text-slate-600'>Signup</a>
-          <button className='block w-50 text-left p-1 py-1.5 px-3 mt-2 mr-24 bg-blue-500 rounded text-white hover:bg-blue-600'>Signout</button>
+          <button className='block w-50 text-left p-1 py-1.5 px-3 mt-2 bg-blue-500 rounded text-white hover:bg-blue-600'>Signout</button>
         </div>
       )}
     </nav>
