a7745954fe7cca86ad15d964dd2db0561a0fbfee
Kuwar20 <kuwarx1@gmail.com>
2024-06-22 22:25:25 +0530

    feat: Update Home page with login and signup links
    The Home page has been updated to include login and signup links. This change enhances the user experience by providing easy access to these important features directly from the Home page.

--- react-redux-tailwind-sinlog/src/pages/Home.jsx
+++ react-redux-tailwind-sinlog/src/pages/Home.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='flex flex-col justify-center items-center h-[80vh] bg-slate-500'>
+      <div className='mb-4'>
+        <h1 className='font-bold'>Welcome to Home page</h1>
+      </div>
+      <div className=' flex flex-col space-y-2'>
+        <Link to="/login" className='text-white hover:underline'>Login</Link>
+        <Link to="/signup" className='text-white hover:underline'>Signup</Link>
+      </div>
+    </div>
   )
 }
 
