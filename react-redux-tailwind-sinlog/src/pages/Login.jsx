351df06bb377c4d3ab19181fa3679f6c372ff047
Kuwar20 <kuwarx1@gmail.com>
2024-06-23 10:41:53 +0530

    feat: Add login form to Login page
    The Login page has been updated to include a login form with email and password inputs. This change enhances the functionality of the Login page by allowing users to enter their credentials and log in to the application.

--- react-redux-tailwind-sinlog/src/pages/Login.jsx
+++ react-redux-tailwind-sinlog/src/pages/Login.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
-    <div>Login</div>
+    <div className='flex justify-center items-center min-h-screen bg-gray-200 px-4'>
+      <form className='bg-white p-8 rounded-lg shadow-md w-full max-w-md md:w-3/4 lg:w-1/2'>
+        <h1 className='text-center text-2xl font-bold mb-6'>Login</h1>
+        <div className='mb-4'>
+          <label className='block text-gray-700 font-bold mb-2'>Email</label>
+          <input type="email"
+            placeholder='Email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className='w-full px-3 py-2 border border-gray-200 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500'
+          />
+        </div>
+        <div className='mb-4'>
+          <label className='block text-gray-700 font-bold mb-2'>Password</label>
+          <input type="password"
+            placeholder='Password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className='w-full px-3 py-2 border border-gray-200 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500'
+          />
+        </div>
+
+        <button type="submit" className='w-full bg-green-600 py-2 rounded-md hover:bg-green-400 font-bold text-white focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50'>Login</button>
+      </form>
+    </div>
   )
 }
 
