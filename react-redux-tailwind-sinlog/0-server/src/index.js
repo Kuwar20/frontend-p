04e48240193a9fee803554c2b6a85c2c90517815
Kuwar20 <kuwarx1@gmail.com>
2024-06-24 19:33:16 +0530

    chore: Add static file serving for frontend build
    The backend server now serves the frontend build files statically, allowing the frontend to be accessed without running it separately. This improves the development workflow and ensures that any changes made to the frontend are reflected in the backend. The frontend build files are located in the "../../dist" directory.

--- react-redux-tailwind-sinlog/0-server/src/index.js
+++ react-redux-tailwind-sinlog/0-server/src/index.js
@@ -8,6 +8,21 @@ connectDB();
 
 app.use(cors());
 
+// connect the frontend dist (build file) here statically on backend, so that we can see the frontend on the backend without having to run the frontend separately
+// one issue is that now the frontend is static so do it in last, any changes in frontend wont show up
+// we are using the path module to join the current directory with the dist folder, if you are unpdating frontend you have to build it again
+// or just comment out the below code
+
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "../../dist")));
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../../dist/index.html"));
+});
+
 app.use(express.json());
 app.use('/api/user',userRoutes);
 
