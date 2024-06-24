310f3f76b45fd20133c7d0227486e62c8a7ceaad
Kuwar20 <kuwarx1@gmail.com>
2024-06-24 12:12:32 +0530

    feat: Add userRoutes for user authentication and registration
    The userRoutes file has been added to handle user authentication and registration. This addition enhances the functionality of the server by providing endpoints for user signup and login, improving the user experience and security of the application.

--- react-redux-tailwind-sinlog/0-server/src/index.js
+++ react-redux-tailwind-sinlog/0-server/src/index.js
@@ -1,8 +1,12 @@
 import express from 'express';
 const app = express();
+import userRoutes from './routes/userRoutes.js';
 
 import { connectDB } from './utils/connDB.js';
 connectDB();
+
+app.use(express.json());
+app.use('/api/user',userRoutes);
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
---
+++ react-redux-tailwind-sinlog/0-server/src/routes/userRoutes.js
@@ -0,0 +1,30 @@
+import express from "express";
+const router = express.Router();
+import User from "../models/users.js";
+
+router.post('/signup', async (req, res) => {
+    const { firstname,lastname, email, password } = req.body;
+    if(!firstname || !lastname || !email || !password){
+        return res.status(400).json({ message: 'All fields are required' });
+    }
+    if(password.length < 6){
+        return res.status(400).json({ message: 'Password must be at least 6 characters' });
+    }
+
+    try {
+        const existingUser = await User.findOne({ email });
+        if(existingUser){
+            return res.status(400).json({ message: 'User already exists' });
+        }
+        const newUser = new User({ firstname, lastname, email, password });
+        await newUser.save();
+        return res.status(201).json({ message: 'User created successfully' });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: 'Internal server error' });
+    }
+});
+
+
+
+export default router;
