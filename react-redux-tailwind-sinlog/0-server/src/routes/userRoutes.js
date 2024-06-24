34b52d2f844921d89cb7e1cf3f81e37a238d741d
Kuwar20 <kuwarx1@gmail.com>
2024-06-24 13:08:31 +0530

    feat: Update userRoutes for user update functionality
    The userRoutes file has been updated to include a new endpoint for updating user information. This addition enhances the functionality of the server by allowing users to update their first name, last name, email, and password.

--- react-redux-tailwind-sinlog/0-server/src/routes/userRoutes.js
+++ react-redux-tailwind-sinlog/0-server/src/routes/userRoutes.js
@@ -5,24 +5,24 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import authenticateToken from "../middlewares/auth.js";
 
-const JWT_SECRET= 'secret';
+const JWT_SECRET = 'secret';
 
 router.post('/signup', async (req, res) => {
-    const { firstname,lastname, email, password } = req.body;
-    if(!firstname || !lastname || !email || !password){
+    const { firstname, lastname, email, password } = req.body;
+    if (!firstname || !lastname || !email || !password) {
         return res.status(400).json({ message: 'All fields are required' });
     }
-    if(password.length < 6){
+    if (password.length < 6) {
         return res.status(400).json({ message: 'Password must be at least 6 characters' });
     }
 
     try {
         const existingUser = await User.findOne({ email });
-        if(existingUser){
+        if (existingUser) {
             return res.status(400).json({ message: 'User already exists' });
         }
         const hashedPassword = await bcrypt.hash(password, 12);
-        const newUser = new User({ firstname, lastname, email, password:hashedPassword });
+        const newUser = new User({ firstname, lastname, email, password: hashedPassword });
         await newUser.save();
         return res.status(201).json({ message: 'User created successfully' });
     } catch (error) {
@@ -33,19 +33,19 @@ router.post('/signup', async (req, res) => {
 
 router.post('/login', async (req, res) => {
     let { email, password } = req.body;
-    if(!email || !password){
+    if (!email || !password) {
         return res.status(400).json({ message: 'All fields are required' });
     }
     try {
         const existingUser = await User.findOne({ email });
-        if(!existingUser){
+        if (!existingUser) {
             return res.status(400).json({ message: 'Invalid credentials1' });
         }
         const isPasswordMatch = await bcrypt.compare(password, existingUser.password);
-        if(!isPasswordMatch){
+        if (!isPasswordMatch) {
             return res.status(400).json({ message: 'Invalid credentials2' });
         }
-        const token = jwt.sign({ email: existingUser.email}, JWT_SECRET, { expiresIn: '1h' });
+        const token = jwt.sign({ email: existingUser.email }, JWT_SECRET, { expiresIn: '1h' });
         return res.status(200).json({ message: 'Login successful', token });
     } catch (error) {
         console.log(error);
@@ -55,16 +55,49 @@ router.post('/login', async (req, res) => {
 
 router.delete('/delete', authenticateToken, async (req, res) => {
     const { email } = req.body;
-    if(!email){
+    if (!email) {
         return res.status(400).json({ message: 'Email is required' });
     }
     try {
         const existingUser = await User.findOne({ email });
-        if(!existingUser){
+        if (!existingUser) {
             return res.status(400).json({ message: 'User not found' });
         }
         await User.deleteOne({ email });
         return res.status(200).json({ message: 'User deleted successfully' });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: 'Internal server error' });
+    }
+});
+
+router.put('/update', authenticateToken, async (req, res) => {
+    const { firstname, lastname, email, password } = req.body; // req.body is the data sent by the client
+    const authenticatedUserEmail = req.user.email; // req.user is the user object decoded from the token
+
+    if (!firstname && !lastname && !email && !password) {
+        return res.status(400).json({ message: 'All fields are required' });
+    }
+    
+    try {
+        const user = await User.findOne({ email: authenticatedUserEmail });
+        if (!user) {
+            return res.status(400).json({ message: 'User not found' });
+        }
+        if (firstname) {
+            user.firstname = firstname;
+        }
+        if (lastname) {
+            user.lastname = lastname;
+        }
+        if (email) {
+            user.email = email;
+        }
+        if (password) {
+            user.password = await bcrypt.hash(password, 12);
+        }
+        await user.save();
+        return res.status(200).json({ message: 'User updated successfully', user });
     } catch (error) {
         console.log(error);
         return res.status(500).json({ message: 'Internal server error' });
