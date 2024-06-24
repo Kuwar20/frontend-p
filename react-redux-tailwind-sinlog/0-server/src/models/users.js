2cf853ef042abdceb19cf4bf8e49f821f378ac98
Kuwar20 <kuwarx1@gmail.com>
2024-06-24 11:37:13 +0530

    feat: Add User model for user data storage
    A new User model has been added to the project to handle the storage of user data. This model includes fields for the user's first name, last name, email, password, and timestamps for creation and updates. This addition enhances the functionality of the server by providing a structured way to store and retrieve user information.

---
+++ react-redux-tailwind-sinlog/0-server/src/models/users.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    firstname: { type: String, required: true },
+    lastname: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    created_at: { type: Date, default: Date.now },
+    updated_at: { type: Date, default: Date.now }
+});
+
+const User = mongoose.model('User', userSchema);
+export default User;
